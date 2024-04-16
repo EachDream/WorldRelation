44c34bea2feff4558a50e0b770116a14194d8a3d
nesb01t <nesb01t@163.com>
2024-04-16 00:44:48 -0700

    update: extract g6

--- components/relation-map/index.tsx
+++ components/relation-map/index.tsx
@@ -1,105 +1,17 @@
 import { useEffect, useRef } from 'react';
 import styles from './index.module.scss';
-import G6 from '@antv/g6';
-import { transformRelationToMap } from '@/utils/relation-to-map';
-import { loadRelationFromFile } from '@/utils/load-relation';
-import { MapData } from '@/types/map';
+import { useRelationMap } from '@/hooks/use-relation-map';
 
 export default function RelationMap() {
   const mapEl = useRef(null);
-  let graph: any = null;
-
-  const data: MapData = transformRelationToMap(loadRelationFromFile());
+  const { init } = useRelationMap();
 
   useEffect(() => {
-    if (!graph && mapEl.current) {
-      graph = new G6.Graph({
-        container: mapEl.current,
-        modes: {
-          default: ['drag-canvas', 'zoom-canvas', 'drag-node'],
-        },
-        layout: {
-          type: 'force',
-          preventOverlap: true,
-          nodeSpacing: 15,
-          edgeStrength: 0.1,
-        },
-
-        // 常用配置
-        fitView: true,
-        animate: true,
-
-        // 默认节点
-        defaultNode: {
-          type: 'text',
-          size: 50,
-          style: {
-            fill: 'white',
-            opacity: 0.25,
-          },
-          labelCfg: {
-            style: {
-              fill: 'black',
-              fontSize: 10,
-            },
-          },
-        },
-
-        // 默认连接
-        defaultEdge: {
-          shape: 'polyline',
-          style: {
-            opacity: 0.15,
-            stroke: 'black',
-          },
-        },
-
-        // hover 效果
-        nodeStateStyles: {
-          hover: {
-            fill: 'lightsteelblue',
-            size: 60, // Increase node size on hover
-          },
-          click: {
-            stroke: '#000',
-            lineWidth: 1,
-            size: 70, // Increase node size on click
-          },
-        },
-        edgeStateStyles: {
-          click: {
-            stroke: 'steelblue',
-          },
-        },
-      });
+    if (mapEl.current) {
+      init(mapEl.current);
     }
-    graph.data(data);
-    graph.render();
-
-    /**
-     * 监听 mouse region
-     */
-    graph.on('node:mouseenter', (e: any) => {
-      const nodeItem = e.item;
-      graph.setItemState(nodeItem, 'hover', true);
-    });
-    graph.on('node:mouseleave', (e: any) => {
-      const nodeItem = e.item;
-      graph.setItemState(nodeItem, 'hover', false);
-    });
-
-    /**
-     * 监听 mouse click
-     */
-    graph.on('node:click', (e: any) => {
-      const clickNodes = graph.findAllByState('node', 'click');
-      clickNodes.forEach((cn: any) => {
-        graph.setItemState(cn, 'click', false);
-      });
-      const nodeItem = e.item;
-      graph.setItemState(nodeItem, 'click', true);
-    });
   }, []);
+
   return (
     <div className={styles.container}>
       <div ref={mapEl} className={styles.map}></div>
